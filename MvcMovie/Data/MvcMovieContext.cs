14e00666b5d7ebb06945032deb06a0e208f94c2d
Thao Pham <phamthao80s@gmail.com>
2022-01-10 13:31:15 +0000

    Global using

--- MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
-
-namespace MvcMovie.Data
+﻿namespace MvcMovie.Data
 {
     public class MvcMovieContext : DbContext
     {
